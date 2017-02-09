4ec5030f24fdb651a12144a890e37f0fa724f550
RickWei <wlh19960621@vip.qq.com>
2017-02-09 23:04:37 +0800

    lab4:part B complete

--- kern/syscall.c
+++ kern/syscall.c
@@ -137,6 +137,12 @@ static int
 sys_env_set_pgfault_upcall(envid_t envid, void *func)
 {
 	// LAB 4: Your code here.
+    struct Env *e;
+    int ret;
+    if (ret=envid2env(envid,&e,1),ret) return ret;
+    e->env_pgfault_upcall=func;
+    return 0;
+    
 	panic("sys_env_set_pgfault_upcall not implemented");
 }
 
@@ -421,6 +427,7 @@ syscall(uint32_t syscallno, uint32_t a1, uint32_t a2, uint32_t a3, uint32_t a4, 
             sys_env_set_status(a1,a2);
             break;
         
+        //challenge
         case SYS_change_pr:
             sys_change_pr(a1);
             break;
@@ -430,8 +437,10 @@ syscall(uint32_t syscallno, uint32_t a1, uint32_t a2, uint32_t a3, uint32_t a4, 
         case SYS_proc_restore:
             sys_proc_restore(a1,(void*)a2);
             break;
-        
-            
+        /////
+        case SYS_env_set_pgfault_upcall:
+            sys_env_set_pgfault_upcall(a1,(void*)a2);
+            break;
         
         default:
             panic("here %x\n",syscallno);
--- lib/fork.c
+++ lib/fork.c
@@ -25,7 +25,8 @@ pgfault(struct UTrapframe *utf)
 	//   (see <inc/memlayout.h>).
 
 	// LAB 4: Your code here.
-
+    if (!((err&FEC_WR)&&(uvpt[PGNUM(addr)]&PTE_COW)))
+        panic("not write or copy-on-write");
 	// Allocate a new page, map it at a temporary location (PFTEMP),
 	// copy the data from the old page to the new page, then move the new
 	// page to the old page's address.
@@ -33,7 +34,14 @@ pgfault(struct UTrapframe *utf)
 	//   You should make three system calls.
 
 	// LAB 4: Your code here.
-
+    addr=ROUNDDOWN(addr, PGSIZE);
+    sys_page_alloc(0,PFTEMP,PTE_W|PTE_U|PTE_P);
+    memcpy(PFTEMP,addr,PGSIZE);
+    sys_page_map(0,PFTEMP,0,addr,PTE_W|PTE_U|PTE_P);
+    sys_page_unmap(0,PFTEMP);
+    return;
+    
+    
 	panic("pgfault not implemented");
 }
 
@@ -54,6 +62,14 @@ duppage(envid_t envid, unsigned pn)
 	int r;
 
 	// LAB 4: Your code here.
+    void *addr=(void*)(pn*PGSIZE);
+    if ((uvpt[pn]&PTE_W)||(uvpt[pn]&PTE_COW)) {
+        sys_page_map(0,addr,envid,addr,PTE_COW|PTE_U|PTE_P);
+        sys_page_map(0,addr,0,addr,PTE_COW|PTE_U|PTE_P);
+    }
+    else sys_page_map(0,addr,envid,addr,PTE_U|PTE_P);
+    return 0;
+    
 	panic("duppage not implemented");
 	return 0;
 }
@@ -78,6 +94,31 @@ envid_t
 fork(void)
 {
 	// LAB 4: Your code here.
+    set_pgfault_handler(pgfault);
+    envid_t envid=sys_exofork();
+    if (envid < 0)
+        panic("sys_exofork: %e", envid);
+    if (envid == 0) {
+        // We're the child.
+        // The copied value of the global variable 'thisenv'
+        // is no longer valid (it refers to the parent!).
+        // Fix it and return 0.
+        thisenv = &envs[ENVX(sys_getenvid())];
+        return 0;
+    }
+    
+    // We're the parent.
+    for (int addr=0; addr<USTACKTOP; addr+=PGSIZE)
+        if ((uvpd[PDX(addr)]&PTE_P)&&(uvpt[PGNUM(addr)]&PTE_P)
+            &&(uvpt[PGNUM(addr)]&PTE_U)) {
+            duppage(envid, PGNUM(addr));
+        }
+    sys_page_alloc(envid,(void *)(UXSTACKTOP-PGSIZE),PTE_U|PTE_W|PTE_P);
+    extern void _pgfault_upcall();
+    sys_env_set_pgfault_upcall(envid, _pgfault_upcall);
+    sys_env_set_status(envid, ENV_RUNNABLE);
+    return envid;
+    
 	panic("fork not implemented");
 }
 
